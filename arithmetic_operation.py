cfb6f5a4cc136abd6ab1b7957064edec60b16b41
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-13 05:31:19 +0000

    ARITHMETIC OPERATION
    Write A Python Program To Perform Different Arithmetic Operations
    
    c=a+b
    p=a-b
    f=a*b
    l=a/b

---
+++ arithmetic_operation.py
@@ -0,0 +1,14 @@
+#Write A Python Program To Perform Different Arithmetic Operations
+
+a=int(input("Enter The  First Element   :"))
+b=int(input("Enter The Second Element   :"))
+
+c=a+b
+p=a-b
+f=a*b
+l=a/b
+
+print("The Addition Of Given Elementd Is       :",c)
+print("The Subtraction Of Given Elementd Is    :",p)
+print("The Multiplication Of Given Elementd Is :",c)
+print("The Division Of Given Elementd Is       :",l)
