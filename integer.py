80332dfc3e57862ec5c5956cfcc226e4ca3a37ab
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-13 05:35:07 +0000

    N+NN+NNN
    #Write A Python Program That Accept Interge Computes The Value of n+nn+nnn

---
+++ integer.py
@@ -0,0 +1,16 @@
+#Write A Python Program That Accept Interge Computes The Value of n+nn+nnn
+
+n=int(input("Enter The Value Of n  :"))
+
+temp=str(n)
+  
+  
+  
+n1=temp+temp
+n2=temp+temp+temp
+value=n+int(n1)+int(n2)
+
+print("Value  Is   :",value)
+   
+     
+     
