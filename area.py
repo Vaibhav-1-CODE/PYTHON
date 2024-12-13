4bd8672dcb75f6d2c9cc27aca4a7830d721c582a
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-13 05:28:49 +0000

    FIND THE AREA
    #Write A Python Program To Find Area  Of Triangle

---
+++ area.py
@@ -0,0 +1,6 @@
+#Write A Python Program To Find Area Fo Triangle
+
+a=int(input("Enter The Base  Of Triangle   :"))
+b=int(input("Enter The Height Of Triangle  :"))
+t=0.5*a*b
+print("The Area Of Triangle Is        : ",t)
