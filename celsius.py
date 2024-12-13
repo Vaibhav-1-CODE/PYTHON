ea31ad9d1e127749d42836d9129a29dfbfed8cf7
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-13 05:33:36 +0000

    CONVERT CELSIUS TO FAHRHRNHEIT
    #Write A Python Python Program To Convert Celsius To Fahrenheit.

---
+++ celsius.py
@@ -0,0 +1,6 @@
+#Write A Python Python Program To Convert Celsius To Fahrenheit.
+
+celsius=int(input("Enter The Celsius   :"))
+
+fahrenheit=(celsius*1.8)+32
+print("The Fahrenheit  is   :",fahrenheit)
