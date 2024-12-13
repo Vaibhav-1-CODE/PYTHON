5bbe923c2bb8ef5c770d3c30822e478086fd4c5c
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-13 05:26:35 +0000

    SWAP THE NUMBERS
    #Write A Python Program To swap two Numbers

---
+++ swap.py
@@ -0,0 +1,11 @@
+#Write A Python Program To swap two Numbers 
+
+a=int(input("Enter The First Number  : "))
+b=int(input("Enter The Second Nember :"))
+
+print("the given numbers is    :",a,b)
+
+temp=a
+a=b
+b=temp
+print("After Swaping Numbers   :",a,b)
