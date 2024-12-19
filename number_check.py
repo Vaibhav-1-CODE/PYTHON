60cad3046e365cc73fc71a3e800b5d126ad5a8d9
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-19 16:38:30 +0000

    CHECK NUMBERS
    # WRITE A PYTHON PROGRAM TO CHECK IF A NUMBER IS POSITIVE,NEGATIVE OR ZERO

---
+++ number_check.py
@@ -0,0 +1,11 @@
+# WRITE A PYTHON PROGRAM TO CHECK IF A NUMBER IS POSITIVE,NEGATIVE OR ZERO
+
+n=int(input("Enter A Number  :"))
+ 
+   
+if(n>0):
+     print([n],"The  Number Is Positive ")
+elif(n==0):
+     print([n],"The  Number Is Zero  ")
+else:
+     print([n],"The  Number Is Negative")  
