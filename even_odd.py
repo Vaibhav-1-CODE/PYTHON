0b1510e9644c8c9ced67de2ce7a80b45955aaba8
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-23 05:05:11 +0000

    EVEN_ODD
    #Write A Python Programming to Check If A Number Is Odd Or Even.

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+#Write A Python Programming to Check If A Number Is Odd Or Even.
+
+n=int (input("Enter A Number : "))
+
+
+if(n%2==0):
+ print(n,"Is A Even Number  ")
+else:
+ print(n,"Is A Odd Number  ")  
