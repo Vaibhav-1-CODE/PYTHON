586d3c7b5a7e09c35b95c65224089d2f3be5885f
Vaibhav-1-CODE <vaibhavsalunkhe019@gmail.com>
2024-12-26 17:13:23 +0000

    CHECK
    #Write A Python Program Check To Check Eligibility For Voting

---
+++ check.py
@@ -0,0 +1,7 @@
+#Write A Python Progrsm To Check To Check Eligibility For Voting 
+
+a=int(input("Enter your Age   :"))
+if(a>=18):
+  print("You Are Eligibile For Voting ")
+else:
+  print("You Are Not Eligibile afor voiting")
